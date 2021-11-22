7fa2da6c2fa5bbb2354a1e5ed639adc7a8962608
ElMehdi Lharoui <59534567+lharouielmehdi@users.noreply.github.com>
2021-11-22 22:59:37 +0100

    test

---
+++ automate-comp/struct.h
@@ -0,0 +1,43 @@
+#pragma once
+#include<stdio.h>
+#include<ctype.h>
+#include<string.h>
+#include<stdlib.h>
+char token_buffer[100];
+typedef enum token_type{
+    mot_clef,oprel,id,nb
+}token;
+char TableTokens[4][50]={"mot_clef","oprel","id","nb"};
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
